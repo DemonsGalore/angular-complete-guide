bad8a2330b1c7990bd5e035ca9fdc6648eca5c5b
Andreas Stopat <dev@demonsgalore.de>
2019-11-19 14:21:52 +0100

    bootstrap fix and and success/warning-alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlert } from './success-alert/success-alert.component';
+import { WarningAlert } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    SuccessAlert,
+    WarningAlert
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './warning-alert.component.html',
+  // add NgbAlertConfig  to the component providers
+  providers: [NgbAlertConfig]
+})
+export class WarningAlert {
+  @Input() public alerts: Array<string> = [];
+
+  constructor(alertConfig: NgbAlertConfig) {
+    // customize default values of alerts used by this component tree
+    alertConfig.type = 'warning';
+    alertConfig.dismissible = false;
+  }
+}
