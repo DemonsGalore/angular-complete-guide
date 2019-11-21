83e974fef5865edb70d4107a5bf6f8526760150b
Andreas Stopat <dev@demonsgalore.de>
2019-11-21 10:56:09 +0100

    chapter 2 - the basics complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -18,7 +19,8 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,4 +5,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent { }
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  username = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Liveserver'];
+  toggleContent = true;
+  timestamps = [];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer = () => {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server with the name "' + this.serverName + '" was created!';
+  }
+
+  onToggle = () => {
+    this.toggleContent = !this.toggleContent;
+    this.timestamps.push(Date.now());
+  }
+}
