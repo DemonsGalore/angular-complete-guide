21e31e4d12804eb8e221dee57795d1d2ff6ca148
Andreas Stopat <dev@demonsgalore.de>
2020-03-15 12:53:06 +0100

    refactor: add lazy loading and preloading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { AuthModule } from './auth/auth.module';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
@@ -21,10 +18,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     BrowserAnimationsModule,
     HttpClientModule,
     MatSnackBarModule,
-    CoreModule,
-    AuthModule,
-    RecipesModule,
-    ShoppingListModule
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
